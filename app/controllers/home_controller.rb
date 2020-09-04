f4c4f91f3fa213e19175396a9d15c739c69cee50
“talha0300” <talhaali0300@gmail.com>
2020-09-04 10:57:45 +0500

    DinerDash: Minor changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!,only: [:show]
   def index
 
   end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def index
+    @users = User.where.not(id: current_user.id)
+
+  end
+end
