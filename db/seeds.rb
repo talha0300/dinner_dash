6ece230f66b02371377f5318eb0bf1c81cc80023
“talha0300” <talhaali0300@gmail.com>
2020-09-16 17:09:55 +0500

    seed data for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  user=User.new(user_name:"Talha",display_name:"Talha Ali",email:"talhaali0300@gmail.com",password:"devdev",role:"admin")
+  user.skip_confirmation!
+  user.save(validate: false)
